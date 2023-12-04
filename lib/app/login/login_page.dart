f704b41baecc673439f754ebb0cd0fd129a8a9cf
Sebastian Lech <lechsebastian99@gmail.com>
2023-12-04 23:45:55 +0100

    Add error handling

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({
     super.key,
   });
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var errorMessage = '';
 
   @override
   Widget build(BuildContext context) {
@@ -32,27 +39,42 @@ class LoginPage extends StatelessWidget {
               ),
               const SizedBox(height: 24),
               TextField(
-                controller: emailController,
+                controller: widget.emailController,
                 decoration: const InputDecoration(label: Text('email')),
               ),
               TextField(
-                controller: passwordController,
+                controller: widget.passwordController,
                 decoration: const InputDecoration(label: Text('password')),
                 obscureText: true,
               ),
-              const SizedBox(height: 24),
+              const SizedBox(height: 12),
+              Text(errorMessage),
+              const SizedBox(height: 12),
               ElevatedButton(
                   onPressed: () async {
                     try {
                       await FirebaseAuth.instance.signInWithEmailAndPassword(
-                        email: emailController.text,
-                        password: passwordController.text,
+                        email: widget.emailController.text,
+                        password: widget.passwordController.text,
                       );
                     } catch (error) {
-                      print(error);
+                      setState(() {
+                        errorMessage = 'Error occured: ${error.toString()}';
+                      });
                     }
                   },
-                  child: const Text('Log in'))
+                  child: const Text('Log in')),
+              const SizedBox(height: 36),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?'),
+                  Text(
+                    ' Register now',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
             ],
           ),
         ),
