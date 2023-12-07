4fe78d17b531b18afafb8c8101ec3c48bd97c667
Sebastian Lech <lechsebastian99@gmail.com>
2023-12-08 00:50:21 +0100

    Add BottomNavigationBar to HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
     required this.user,
@@ -10,22 +10,54 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('You are logged in as ${user.email}'),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-              },
-              child: const Text('Log out'),
-            ),
-          ],
-        ),
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Center(
+            child: Text('Birthdays'),
+          );
+        }
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('You are logged in as ${widget.user.email}'),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                },
+                child: const Text('Log out'),
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Birthdays',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'My account',
+          ),
+        ],
       ),
     );
   }
