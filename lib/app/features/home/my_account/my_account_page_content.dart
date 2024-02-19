569277f5f1a06d5966d4064c773ba62b56ab5c4e
Sebastian Lech <lechsebastian99@gmail.com>
2024-02-19 22:29:14 +0100

    Change the logout way

--- lib/app/features/home/my_account/my_account_page_content.dart
+++ lib/app/features/home/my_account/my_account_page_content.dart
@@ -1,4 +1,5 @@
 import 'package:birthdays_reminder_app/app/cubit/root_cubit.dart';
+import 'package:birthdays_reminder_app/themes/my_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,19 +13,28 @@ class MyAccountPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('You are logged in as $email'),
-          const SizedBox(height: 24),
-          ElevatedButton(
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: MyColor().myColor,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
             onPressed: () async {
               context.read<RootCubit>().signOut();
             },
-            child: const Text('Log out'),
+            icon: const Icon(Icons.logout),
           ),
         ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You are logged in as $email'),
+            const SizedBox(height: 24),
+          ],
+        ),
       ),
     );
   }
